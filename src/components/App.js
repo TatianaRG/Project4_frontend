91fd58183fbc325e1d52048882d500d326e3c0ee
TatianaGuzun <tatiana.guzun@reachplc.com>
2022-04-19 15:36:18 +0100

    innitial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import '../styles/style.scss';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Hello world</h1>} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
